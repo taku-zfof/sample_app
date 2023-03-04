3c0a76dffab52b9f2bd1773b7b1fa99940b65e5d
EC2 Default User <ec2-user@ip-172-31-39-5.ap-northeast-1.compute.internal>
2023-03-04 04:25:17 +0000

    [finish]3-12

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,7 +6,8 @@ class ListsController < ApplicationController
   def create
     @list=List.new(list_params)
     if @list.save
-      redirect_to list_path(@list.id)
+      flash[:notice] = "投稿が成功しました"
+      redirect_to lists_path
     else
       render ("lists/new")
     end
@@ -28,6 +29,7 @@ class ListsController < ApplicationController
   def update
     list=List.find(params[:id])
     list.update(list_params)
+    flash[:notice] = "投稿が編集されました"
     redirect_to list_path(list.id)
   end
   def destroy
