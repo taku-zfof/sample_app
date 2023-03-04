7ceb95d2c5e9120a856ceb53cc2cc1caad3a90bf
EC2 Default User <ec2-user@ip-172-31-39-5.ap-northeast-1.compute.internal>
2023-03-04 00:52:29 +0000

    [finish]3-7

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
 
   get "lists/:id" => "lists#show",as:"list"
   get '/' => "homes#top"
+  delete "lists/:id" => "lists#destroy" ,as:"destroy_list"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
